6b55c5b14938e928120ef3db32e8f6e43ddcc7c8
Josh McLain <joshmclain45@gmail.com>
2023-03-30 05:44:37 -0700

    📦 NEW:  refactor card

--- client/src/components/TarotPageComponents/ReadingsLayout.tsx
+++ client/src/components/TarotPageComponents/ReadingsLayout.tsx
@@ -1,5 +1,31 @@
 import React, { useState } from 'react';
-import CardBack from '../../assets/CardBacks.jpg';
+import cardBack from '../../assets/CardBacks.jpg';
+
+type CardProps = {
+    labelTag: string;
+    faceSrc?: string;
+    isLayered?: boolean;
+    children?: JSX.Element;
+    rotate90?: boolean;
+    inverted?: boolean;
+};
+const Card = ({ labelTag, faceSrc, isLayered, children, rotate90, inverted }: CardProps): JSX.Element => (
+    <div className="relative text-white">
+        <span
+            className={`absolute z-30 top-0 left-0 bg-black/80 p-1 block leading-4 rounded text-sm${
+                faceSrc ? ' hidden' : ''
+            }${rotate90 ? ' -rotate-90' : ''}`}
+        >
+            {labelTag}
+        </span>
+        <img
+            id={`card-${labelTag}`}
+            src={faceSrc ? faceSrc : cardBack}
+            className={`h-24 grayscale ${inverted ? ' rotate-180' : ''}`}
+        />
+        {isLayered && children}
+    </div>
+);
 
 const ReadingsLayout = (): JSX.Element => {
     const [readingType, setReadingType] = useState<string>('');
@@ -8,64 +34,51 @@ const ReadingsLayout = (): JSX.Element => {
 
     return (
         <div className="h-screen bg-neutral-800">
-            <nav className="bg-neutral-600">nav</nav>
-            <div id="info" className="container mx-auto text-center p-4 text-white">
-                <ul className="inline-block p-2">
-                    <li>1 you /the question the situation</li>
-                    <li>2 what crosses</li>
-                    <li>3 best of circumstances</li>
-                    <li>4 the foundation</li>
-                    <li>5 the past</li>
-                </ul>
-                <ul className="inline-block p-2">
-                    <li>6 the near future</li>
-                    <li>7 you</li>
-                    <li>8 environment</li>
-                    <li>9 hope or fears</li>
-                    <li>10 the outcome</li>
-                </ul>
-            </div>
-            <div className="container mx-auto border flex justify-center items-center">
-                <span className="">
-                    <div className="grid gap-x-9 gap-y-4 grid-cols-3 grid-rows-3">
-                        <div className=""></div>
-                        <div className="">
-                            <img id="card-5" src={CardBack} className="h-32 grayscale" />
-                        </div>
-                        <div className=""></div>
-                        <div className="">
-                            <img id="card-4" src={CardBack} className="h-32 grayscale" />
-                        </div>
-                        <div className="relative">
-                            <img id="card-1" src={CardBack} className="h-32 grayscale" />
-                            <img id="card-2" src={CardBack} className="h-32 grayscale absolute rotate-90 inset-0" />
-                        </div>
-                        <div className="">
-                            <img id="card-6" src={CardBack} className="h-32 grayscale" />
-                        </div>
-                        <div className=""></div>
-                        <div className="">
-                            <img id="card-3" src={CardBack} className="h-32 grayscale" />
-                        </div>
-                        <div className=""></div>
+            <div className="container mx-auto border-4 bg-neutral-900 p-3">
+                <h2 className="text-2xl font-bold text-neutral-600">Celtic Cross</h2>
+                <div className="flex flex-col items-center">
+                    <ol className="p-2 text-neutral-500 mr-5 space-y-4 hidden">
+                        <li>1. the question the situation</li>
+                        <li>2. what crosses</li>
+                        <li>3. best of circumstances</li>
+                        <li>4. the foundation</li>
+                        <li>5. the past</li>
+                        <li>6. the near future</li>
+                        <li>7. you</li>
+                        <li>8. environment</li>
+                        <li>9. hope or fears</li>
+                        <li>10. the outcome</li>
+                    </ol>
+                    <div className="flex justify-center items-center">
+                        <span className="">
+                            <div className="grid gap-x-9 gap-y-4 grid-cols-3 grid-rows-3 text-white">
+                                <div className=""></div>
+                                <div>
+                                    <Card labelTag="5" faceSrc={undefined} />
+                                </div>
+                                <div className=""></div>
+                                <Card labelTag="4" faceSrc={undefined} />
+                                <Card labelTag="1" faceSrc={undefined} isLayered={true}>
+                                    <span className="absolute rotate-90 inset-0">
+                                        <Card labelTag="2" faceSrc={undefined} rotate90={true} />
+                                    </span>
+                                </Card>
+                                <Card labelTag="6" faceSrc={undefined} />
+                                <div className=""></div>
+                                <Card labelTag="3" faceSrc={undefined} />
+                                <div className=""></div>
+                            </div>
+                        </span>
+                        <span className="ml-4">
+                            <div className="grid gap-4 grid-cols-1 grid-row-4 text-white">
+                                <Card labelTag="10" faceSrc={undefined} />
+                                <Card labelTag="9" faceSrc={undefined} />
+                                <Card labelTag="8" faceSrc={undefined} />
+                                <Card labelTag="7" faceSrc={undefined} />
+                            </div>
+                        </span>
                     </div>
-                </span>
-                <span className="ml-4">
-                    <div className="grid gap-4 grid-cols-1 grid-row-4">
-                        <div className="">
-                            <img id="card-10" src={CardBack} className="h-32 grayscale" />
-                        </div>
-                        <div className="">
-                            <img id="card-9" src={CardBack} className="h-32 grayscale" />
-                        </div>
-                        <div className="">
-                            <img id="card-8" src={CardBack} className="h-32 grayscale" />
-                        </div>
-                        <div className="">
-                            <img id="card-7" src={CardBack} className="h-32 grayscale" />
-                        </div>
-                    </div>
-                </span>
+                </div>
             </div>
         </div>
     );
