63c243aa6a5d728f27c07719e2cb8f71b472bd61
Josh McLain <joshmclain45@gmail.com>
2023-03-30 14:08:17 -0700

    📦 NEW: week ahead layout

--- client/src/components/TarotPageComponents/PositionList.tsx
+++ client/src/components/TarotPageComponents/PositionList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { v4 as uuid } from 'uuid';
 
 export const PositionList = ({ list }: { list: string[] }): JSX.Element => (
-    <ol className="p-2 float-left md:float-none text-neutral-500 text-xs md:text-base md:space-y-2 md:mr-10 list-decimal">
+    <ol className="p-2 float-left md:float-none text-neutral-500 text-xs md:text-base md:space-y-2 md:mr-24 list-decimal">
         {list.map((item) => (
             <li key={uuid()}>{item}</li>
         ))}
--- client/src/components/TarotPageComponents/ReadingsLayout.tsx
+++ client/src/components/TarotPageComponents/ReadingsLayout.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { PositionList } from './PositionList';
 import { Card } from './Card';
 
-export const CROSS_LIST_MEANING = [
+const CROSS_LIST_MEANING = [
     'Question / Situation',
     'What crosses you',
     'Best of Circumstances',
@@ -14,6 +14,16 @@ export const CROSS_LIST_MEANING = [
     'Hopes or Fears',
     'The Outcome',
 ];
+const WEEK_LIST_MEANING = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+    'Significator',
+];
 const ReadingsLayout = (): JSX.Element => {
     const [readingType, setReadingType] = useState<string>('');
     console.log('🚀 ~ file: ReadingsLayout.tsx:7 ~ ReadingsLayout ~ setReadingType:', setReadingType);
@@ -21,13 +31,13 @@ const ReadingsLayout = (): JSX.Element => {
 
     return (
         <div className="h-screen bg-neutral-800">
-            <div className="container mx-auto border-4 border-neutral-400 bg-neutral-900 p-3">
-                <h2 className="text-2xl md:text-4xl font-bold text-neutral-500">Celtic Cross</h2>
+            <div className="container mx-auto border-4 border-neutral-500 bg-neutral-900 p-3">
+                <h2 className="text-2xl md:text-4xl font-bold text-neutral-400">Celtic Cross</h2>
                 <div className="md:flex md:justify-center md:flex-row md:items-center md:pb-6">
                     <PositionList list={CROSS_LIST_MEANING} />
                     <div className="flex justify-center items-center">
                         <span className="">
-                            <div className="grid gap-x-5 sm:gap-x-7 md:gap-x-9 gap-y-2 md:gap-y-4 grid-cols-3 grid-rows-3 text-white">
+                            <div className="grid gap-x-5 sm:gap-x-7 md:gap-x-9 gap-y-2 md:gap-y-4 grid-cols-3 grid-rows-3">
                                 <div className=""></div>
                                 <div>
                                     <Card labelTag="5" faceSrc={undefined} />
@@ -51,6 +61,43 @@ const ReadingsLayout = (): JSX.Element => {
                                 <Card labelTag="9" faceSrc={undefined} />
                                 <Card labelTag="8" faceSrc={undefined} />
                                 <Card labelTag="7" faceSrc={undefined} />
+                            </div>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div className="container mx-auto border-4 border-neutral-500 bg-neutral-900 p-3">
+                <h2 className="text-2xl md:text-4xl font-bold text-neutral-400">Week Ahead</h2>
+                <div className="md:flex md:justify-center md:flex-row md:items-center md:pb-6">
+                    <PositionList list={WEEK_LIST_MEANING} />
+                    <div className="flex flex-col justify-center items-center">
+                        <span className="">
+                            <div className="grid gap-x-4 sm:gap-x-4 md:gap-x-8 gap-y-4 md:gap-y-8 grid-cols-3 grid-rows-2">
+                                <div>
+                                    <Card labelTag="6" faceSrc={undefined} />
+                                </div>
+                                <div className="-translate-y-4 md:-translate-y-8">
+                                    <Card labelTag="7" faceSrc={undefined} />
+                                </div>
+                                <div>
+                                    <Card labelTag="1" faceSrc={undefined} />
+                                </div>
+
+                                <div>
+                                    <Card labelTag="5" faceSrc={undefined} />
+                                </div>
+                                <div className="-translate-y-4 md:-translate-y-8">
+                                    <Card labelTag="8" faceSrc={undefined} />
+                                </div>
+                                <div>
+                                    <Card labelTag="2" faceSrc={undefined} />
+                                </div>
+                            </div>
+                        </span>
+                        <span className="mt-4 md:mt-6">
+                            <div className="grid gap-4 md:gap-8 grid-cols-2 grid-row-1 text-white">
+                                <Card labelTag="4" faceSrc={undefined} />
+                                <Card labelTag="3" faceSrc={undefined} />
                             </div>
                         </span>
                     </div>
